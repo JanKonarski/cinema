ce509a8d74b66dc1a1b4afd8c3fe870881e32f2c
jan <jankonarski888@gmail.com>
2022-06-06 16:43:22 +0200

    Base structure

--- script/schema.sql
+++ script/schema.sql
@@ -3,31 +3,74 @@ USE cinema;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
-    id          VARCHAR(36) PRIMARY KEY     /* GUID */
+    id          VARCHAR(36) NOT NULL     /* GUID */
 ,   name        VARCHAR(255) NOT NULL
 ,   password    VARCHAR(98) NOT NULL
 ,   email       VARCHAR(255) DEFAULT NULL
 ,   registered  BOOLEAN DEFAULT false
-);
-
-CREATE TABLE ticket (
-    userID
+,   PRIMARY KEY (id)
 );
 
 CREATE TABLE transaction (
+    id          VARCHAR(36) NOT NULL
+,   timestamp   DATETIME NOT NULL
+,   confirmed   BOOLEAN NOT NULL DEFAULT false
+,   cardNumber  NUMBER(16) NOT NULL
+,   PRIMARY KEY (id)
+);
 
+CREATE TABLE ticket (
+    id              VARCHAR(36) NOT NULL
+,   userID          VARCHAR(36) NOT NULL
+,   transactionID   VARCHAR(36) NOT NULL
+,   showID          VARCHAR(36) NOT NULL
+,   timestamp       DATETIME NOT NULL
+,   seat            VARCHAR(6) NOT NULL
+,
+,   PRIMARY KEY (id)
+,   FOREIGN KEY (userID)        REFERENCES user(id)
+,   FOREIGN KEY (transactionID) REFERENCES transaction(id)
 );
 
 CREATE TABLE show (
-
+    id          VARCHAR(36) NOT NULL
+,   date        DATE NOT NULL
+,   timeStart   TIME NOT NULL
+,   timeEnd     TIME NOT NULL
+,   room        TINYINT NOT NULL
+,   movieID     VARCHAR(36) NOT NULL
+,
+,   PRIMARY KEY (id)
+,   FOREIGN KEY (movieID) REFERENCES movie(id)
 );
 
 CREATE TABLE movie (
-
+    id          VARCHAR(36) PRIMARY KEY
+,   name        VARCHAR(255) NOT NULL
+,   category    ENUM(
+        'action'
+    ,   'comedy'
+    ,   'drama'
+    ,   'fantasy'
+    ,   'horror'
+    ,   'mystery'
+    ,   'romance'
+    ,   'thriller'
+    ,   'western'
+    )   NOT NULL
+,   description VARCHAR(1024) NOT NULL
+,
 );
 
 CREATE TABLE comment (
-
+    id          VARCHAR(36) NOT NULL
+,   userID      VARCHAR(36) NOT NULL
+,   movieID     VARCHAR(36) NOT NULL
+,   text        VARCHAR(2048) DEFAULT NULL
+,   rate        SMALLINT NOT NULL
+,   PRIMARY KEY (id)
+,   FOREIGN KEY (userID) REFERENCES user(id)
+,   FOREIGN KEY (movieID) REFERENCES movie(id)
 );
 
 CREATE TABLE goods (
---
+++ src/html/room.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <style>
+        .room {
+            width: auto;
+        }
+
+        .screen {
+            width: 500px;
+			height: 6px;
+	        background-color: #505050;
+        }
+
+        table {
+            margin-top: 75px;
+        }
+
+        .seat {
+	        width: 25px;
+	        height: 25px;
+            margin: 2px;
+            text-align: center;
+            color: white;
+            vertical-align: middle;
+            cursor: grab;
+        }
+
+        .free {
+            background-color: green;
+        }
+
+        .select {
+            background-color: orange;
+        }
+
+        .occupied {
+            background-color: gray;
+        }
+
+        .transition {
+	        width: 45px;
+	        height: 15px;
+        }
+	</style>
+	<script>
+
+	</script>
+</head>
+<body>
+	<div class="room">
+		<div class="screen"></div>
+		<table class="seats">
+<?php
+	for ($i=1; $i<11; $i++) {
+		print('<tr>');
+		for ($j=1; $j<9; $j++)
+			print('<td><div class="seat free" id="' .('r'.$i.'c'.$j). '">' .($j). '</div></td>');
+		print('<td><div class="transition"></div></td>');
+		for ($j=1; $j<9; $j++)
+			print('<td><div class="seat free" id="' .('r'.$i.'c'.$j). '">' .(8+$j). '</div></td>');
+		print('</tr>');
+	}
+?>
+		</table>
+        <input type="button" id="check" value="check"/>
+        <input type="submit" id="submit" value="Next"/>
+	</div>
+	<script>
+        $('.seat').click(function(){
+            if ($(this).hasClass('occupied')) {
+                alert('This seat is occupied');
+                return;
+            } else {
+                if ($(this).hasClass('free')) {
+                    $(this).removeClass('free');
+                    $(this).addClass('select');
+                } else {
+                    $(this).removeClass('select');
+                    $(this).addClass('free');
+                }
+            }
+        });
+
+        $('#check').click(function(){
+            var list = document.getElementsByClassName('select');
+            var text = '';
+            $.each(list, function(index, item){
+                text += item.id + '\n';
+            });
+            alert(text);
+        });
+
+        $('input#submit').click(function(){
+            $.ajax({
+                url: '',
+                type: 'post',
+                dataType: 'json',
+                data: '',
+                success: function(data) {
+
+                },
+                error: function() {
+
+                }
+            });
+            alert();
+        });
+	</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
